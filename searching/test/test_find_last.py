37424445a2800405c1ecf872a134951085350b6f
Sal Tamay <sal.tamay@outlook.com>
2022-05-07 01:59:42 -0400

    Last Index of Target Implement an iterative algorithm to find the last index of a target value in a sorted array.

---
+++ searching/test/test_find_last.py
@@ -0,0 +1,22 @@
+from searching.find_last import find_last
+
+
+def test_find_last_01():
+    arr = [1, 3, 5, 7, 7, 7, 8, 11, 12, 13, 14, 15]
+    target = 7
+
+    assert find_last(arr, target) == 5
+
+
+def test_find_last_02():
+    arr = [1, 3, 5, 7, 7, 7, 8, 11, 12, 13, 14, 15]
+    target = 9
+
+    assert find_last(arr, target) == -1
+
+
+def test_find_last_03():
+    arr = [1]
+    target = 1
+
+    assert find_last(arr, target) == 0
